8e8ec985a350bf4cdcb6158c3880a8541d795392
Will Fisher <will@teknofire.net>
2012-01-13 16:52:32 -0900

    Adding polar projection osm_overlay layer

--- layers/osm.js
+++ layers/osm.js
@@ -18,6 +18,25 @@ Gina.Layers.define('TILE.EPSG:3338.OSM', {
 });
 
 /**
+ * Name: TILE.EPSG:3572.OSM_OVERLAY
+ * Projection: EPSG:3572 Polar Projection
+ * OpenStreetMap overlay in a polar projection
+ */
+Gina.Layers.define('TILE.EPSG:3572.OSM_OVERLAY', {
+  name: 'OpenStreetMap - Roads & Cities',
+  type: Gina.Layers.Types.TILE,
+  url: 'http://tiles.gina.alaska.edu/test/tilesrv/osm-google-ol-3572/tile/${x}/${y}/${z}',
+  layerOptions: {
+    type: 'jpeg',
+    transitionEffect: 'resize',
+    wrapDateLine: false,
+    visibility: true,
+    isBaseLayer: false,
+    attribution: '(c) <a href="http://www.openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+  }
+});
+
+/**
  * Name: TILE.EPSG:3338.OSM_OVERLAY
  * Projection: EPSG:3338 Alaskan Albers
  * OpenStreetMap road and city overlay in Alaskan Albers projection
