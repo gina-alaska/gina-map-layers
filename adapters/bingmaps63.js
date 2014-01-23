6a4cfa3ee066c07ce6d5a1696c1205f25be105b3
Will Fisher <will@teknofire.net>
2014-01-23 11:34:36 -0900

    adding all of the open weather map tile layers

--- adapters/bingmaps63.js
+++ adapters/bingmaps63.js
@@ -483,7 +483,7 @@ Gina.Layers.define('TILE.EPSG:3857.OWM_CLOUDS', {
     wrapDateLine: true,
     visibility: false,
     isBaseLayer: false,
-    opacity: 0.7,
+    opacity: 0.5,
     attribution: '(c) <a href="http://www.openweathermap.org">Map data &copy; OpenWeatherMap</a>'
   }
 });
@@ -503,10 +503,111 @@ Gina.Layers.define('TILE.EPSG:3857.OWM_PRECIP', {
     wrapDateLine: true,
     visibility: false,
     isBaseLayer: false,
-    opacity: 0.7,
+    opacity: 0.5,
     attribution: '(c) <a href="http://www.openweathermap.org">Map data &copy; OpenWeatherMap</a>'
   }
-});/**
+});
+
+/**
+ * Name: TILE.EPSG:3857.OWM_SNOW
+ * Projection: EPSG:3857 Web Mercator
+ * OpenWeatherMap Snow Overlay in web mercator
+ */
+Gina.Layers.define('TILE.EPSG:3857.OWM_SNOW', {
+  name: 'OpenWeatherMap Snow',
+  type: Gina.Layers.Types.TILE,
+  url: 'http://${s}.tile.openweathermap.org/map/snow/${z}/${x}/${y}.png',
+  layerOptions: {
+    type: 'png',
+    transitionEffect: 'resize',
+    wrapDateLine: true,
+    visibility: false,
+    isBaseLayer: false,
+    opacity: 0.5,
+    attribution: '(c) <a href="http://www.openweathermap.org">Map data &copy; OpenWeatherMap</a>'
+  }
+});
+
+/**
+ * Name: TILE.EPSG:3857.OWM_TEMP
+ * Projection: EPSG:3857 Web Mercator
+ * OpenWeatherMap Temperature Overlay in web mercator
+ */
+Gina.Layers.define('TILE.EPSG:3857.OWM_TEMP', {
+  name: 'OpenWeatherMap Temperature',
+  type: Gina.Layers.Types.TILE,
+  url: 'http://${s}.tile.openweathermap.org/map/temp/${z}/${x}/${y}.png',
+  layerOptions: {
+    type: 'png',
+    transitionEffect: 'resize',
+    wrapDateLine: true,
+    visibility: false,
+    isBaseLayer: false,
+    opacity: 0.5,
+    attribution: '(c) <a href="http://www.openweathermap.org">Map data &copy; OpenWeatherMap</a>'
+  }
+});
+
+/**
+ * Name: TILE.EPSG:3857.OWM_WIND
+ * Projection: EPSG:3857 Web Mercator
+ * OpenWeatherMap Wind speed Overlay in web mercator
+ */
+Gina.Layers.define('TILE.EPSG:3857.OWM_WIND', {
+  name: 'OpenWeatherMap Wind Speed',
+  type: Gina.Layers.Types.TILE,
+  url: 'http://${s}.tile.openweathermap.org/map/wind/${z}/${x}/${y}.png',
+  layerOptions: {
+    type: 'png',
+    transitionEffect: 'resize',
+    wrapDateLine: true,
+    visibility: false,
+    isBaseLayer: false,
+    opacity: 0.5,
+    attribution: '(c) <a href="http://www.openweathermap.org">Map data &copy; OpenWeatherMap</a>'
+  }
+});
+
+/**
+ * Name: TILE.EPSG:3857.OWM_PRESSURE
+ * Projection: EPSG:3857 Web Mercator
+ * OpenWeatherMap Sea level pressure Overlay in web mercator
+ */
+Gina.Layers.define('TILE.EPSG:3857.OWM_PRESSURE', {
+  name: 'OpenWeatherMap Sea level pressure contour',
+  type: Gina.Layers.Types.TILE,
+  url: 'http://${s}.tile.openweathermap.org/map/pressure/${z}/${x}/${y}.png',
+  layerOptions: {
+    type: 'png',
+    transitionEffect: 'resize',
+    wrapDateLine: true,
+    visibility: false,
+    isBaseLayer: false,
+    opacity: 0.5,
+    attribution: '(c) <a href="http://www.openweathermap.org">Map data &copy; OpenWeatherMap</a>'
+  }
+});
+
+/**
+ * Name: TILE.EPSG:3857.OWM_PRESSURE_CNTR
+ * Projection: EPSG:3857 Web Mercator
+ * OpenWeatherMap Sea level pressure contours Overlay in web mercator
+ */
+Gina.Layers.define('TILE.EPSG:3857.OWM_PRESSURE_CNTR', {
+  name: 'OpenWeatherMap Sea level pressure contour',
+  type: Gina.Layers.Types.TILE,
+  url: 'http://${s}.tile.openweathermap.org/map/pressure_cntr/${z}/${x}/${y}.png',
+  layerOptions: {
+    type: 'png',
+    transitionEffect: 'resize',
+    wrapDateLine: true,
+    visibility: false,
+    isBaseLayer: false,
+    opacity: 0.5,
+    attribution: '(c) <a href="http://www.openweathermap.org">Map data &copy; OpenWeatherMap</a>'
+  }
+});
+/**
  * Name: TILE.EPSG:3857.SHADEDRELIEF
  * Projection: EPSG:3857 Google Mercator Projection
  * Gray scale shaded relief base layer
